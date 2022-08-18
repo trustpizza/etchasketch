1c5622bd5f9d70dffa5f0356d062cfb29da537eb
Axel <108423732+trustpizza@users.noreply.github.com>
2022-08-18 18:43:18 -0500

    Add resizeability, add color randomization

--- scripts.js
+++ scripts.js
@@ -1,9 +1,7 @@
-// Create a div within container that holds all pixels
-
 const container = document.querySelector('#container');
 
-let gridSize = 500;
-let columnLength = 16;
+let gridSize = 800;
+let columnLength = 10;
 let pixelCount = columnLength * columnLength;
 let pixelSize = gridSize / columnLength;
 
@@ -11,29 +9,62 @@ const button = document.createElement('button');
 button.innerHTML = 'Click Me!';
 button.addEventListener('click', () => {
     columnLength = prompt('Choose desired column length');
-    pixelCount = columnLength * columnLength;
-    pixelSize = gridSize / columnLength;
-    createGrid(gridSize, columnLength, pixelSize)
+    if (columnLength <= 100) {
+        pixelCount = columnLength * columnLength;
+        pixelSize = gridSize / columnLength;
+        newGrid(gridSize, columnLength, pixelSize)
+    } else {
+        columnLength = prompt('Number must be less than 100')
+        pixelCount = columnLength * columnLength;
+        pixelSize = gridSize / columnLength;
+        newGrid(gridSize, columnLength, pixelSize)
+    }
+
 })
 container.appendChild(button);
 
+function randomColor() {
+    let color = Math.floor(Math.random() * 16777215).toString(16);
+    return color;
+}
+
 function createGrid(gridSize, columnLength, pixelSize) {
-    const grid = document.createElement('div');
+    let grid;
+    grid = document.createElement('div');
+    grid.classList.add('grid')
     grid.style.cssText = `display: grid; height: ${gridSize}px; width: ${gridSize}px; grid-template-columns: repeat(${columnLength}, ${pixelSize}px [col-start]);`;
     container.appendChild(grid);
     for (let i = 0; i < pixelCount; i++) {
         const pixel = document.createElement('div');
+        pixel.classList.add('pixel')
         pixel.style.cssText = `border: 1px solid black; height: ${pixelSize}px; width: ${pixelSize}px; padding: 0px;`
         pixel.addEventListener('mouseover', () => {
-            pixel.style.backgroundColor = 'black'
+            pixel.style.backgroundColor = `#${randomColor()}`
         });
         pixel.addEventListener('mouseout', () => {
             pixel.style.cssText = `border: 1px solid black; height: ${pixelSize}px; width: ${pixelSize}px; padding: 0px;`
         });
         grid.appendChild(pixel);
     }
-    
+}
+
+function newGrid(gridSize, columnLength, pixelSize) {
+    const grid = document.querySelector('.grid');
+    grid.innerHTML = '';
+    grid.classList.add('grid')
+    grid.style.cssText = `display: grid; height: ${gridSize}px; width: ${gridSize}px; grid-template-columns: repeat(${columnLength}, ${pixelSize}px [col-start]);`;
+    container.appendChild(grid);
+    for (let i = 0; i < pixelCount; i++) {
+        const pixel = document.createElement('div');
+        pixel.style.cssText = `border: 1px solid black; height: ${pixelSize}px; width: ${pixelSize}px; padding: 0px;`
+        pixel.addEventListener('mouseover', () => {
+            pixel.style.backgroundColor = `#${randomColor()}`
+        });
+        pixel.addEventListener('mouseout', () => {
+            pixel.style.cssText = `border: 1px solid black; height: ${pixelSize}px; width: ${pixelSize}px; padding: 0px;`
+        }); 
+        grid.appendChild(pixel);
+    }
 }
 
 createGrid(gridSize, columnLength, pixelSize);
-
