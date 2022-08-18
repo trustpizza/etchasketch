6d06833df66f474fb3e8905aeb941e4ce34f11cb
Axel <108423732+trustpizza@users.noreply.github.com>
2022-08-18 08:49:38 -0500

    Make grid size dynamic

--- scripts.js
+++ scripts.js
@@ -2,33 +2,24 @@
 
 const container = document.querySelector('#container');
 
-const button = document.createElement('button'); //Button determines size of Grid
-button.classList.add('button');
-button.innerHTML = "How large would you like your grid?";
-button.addEventListener('click', () => {
-    let size = prompt('Type in the height of your grid');
-    let area = size * size;
-    let pixelHeight = 800 / size;
-    createGrid(size, area);
-    
-})
-container.appendChild(button)
+let gridSize = 500;
+let columnLength = 16;
+let pixelCount = columnLength * columnLength;
+let pixelSize = gridSize / columnLength;
 
-function createGrid(size, area, pixelHeight) {
-    const grid = document.createElement('div');
-    grid.style.cssText = `border: 1px solid black; display: grid; height: 800px; width: 800px; grid-template-columns: repeat(${size}, ${pixelHeight}px [col-start]);`;
-    container.appendChild(grid);
-    for (let i = 0; i < area; i++) {
-        const pixel = document.createElement('div');
-        pixel.style.cssText = `border: 1px solid black; height: ${pixelHeight}px; width: ${pixelHeight}px; padding: 0px;`
-        pixel.addEventListener('mouseover', () => {
-            pixel.style.backgroundColor = 'black'
-        });
-        pixel.addEventListener('mouseout', () => {
-            pixel.style.cssText = `border: 1px solid black; height: ${pixelHeight}px; width: ${pixelHeight}px; padding: 0px;`
-        });
-        grid.appendChild(pixel);
-    }
+
+const grid = document.createElement('div');
+grid.style.cssText = `display: grid; height: ${gridSize}px; width: ${gridSize}px; grid-template-columns: repeat(${columnLength}, ${pixelSize}px [col-start]);`;
+container.appendChild(grid);
+for (let i = 0; i < pixelCount; i++) {
+    const pixel = document.createElement('div');
+    pixel.style.cssText = `border: 1px solid black; height: ${pixelSize}px; width: ${pixelSize}px; padding: 0px;`
+    pixel.addEventListener('mouseover', () => {
+        pixel.style.backgroundColor = 'black'
+    });
+    pixel.addEventListener('mouseout', () => {
+        pixel.style.cssText = `border: 1px solid black; height: ${pixelSize}px; width: ${pixelSize}px; padding: 0px;`
+    });
+    grid.appendChild(pixel);
 }
 
-createGrid(16, 256, 40)
